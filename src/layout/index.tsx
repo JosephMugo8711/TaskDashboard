a5f1f7124f3b66de1c4f1ba0caf80abdebe04a0b
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-10 12:40:25 +0300

    Navbar

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router"
+import Navbar from "../components/Navbar"
 
 const Layout = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="relative w-screen h-screen">
+        <Navbar />
+        <div className="md:pl-[250px] pl-[60px] pr-[20px] pt-[70px] w-full h-full overflow-y-auto">
+			<Outlet />
+		</div>
     </div>
   )
 }
